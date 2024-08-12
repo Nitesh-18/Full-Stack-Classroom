b06585f0dadc67c795add8fbbd0599af1502a368
Nitesh <niteshranjankar5@gmail.com>
2024-08-13 00:09:01 +0530

    User Model added

---
+++ backend/src/models/User.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Principal", "Teacher", "Student"],
+      required: true,
+    },
+    classroomId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Classroom",
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
