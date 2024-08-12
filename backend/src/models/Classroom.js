e6b6ccba82694713e119f449d38a313cf9486b24
Nitesh <niteshranjankar5@gmail.com>
2024-08-13 00:13:06 +0530

    Classroom model added

---
+++ backend/src/models/Classroom.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const classroomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true,
+  },
+  endTime: {
+    type: String,
+    required: true,
+  },
+  days: {
+    type: [String],
+    required: true,
+  },
+  teacherId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: false,
+  },
+}, { timestamps: true });
+
+export const Classroom = mongoose.model('Classroom', classroomSchema);
