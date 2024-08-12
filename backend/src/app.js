88472e0149e809cdd1f0be9d3ca1ea27c4878af4
Nitesh <niteshranjankar5@gmail.com>
2024-08-13 01:11:14 +0530

    "Rename 'router' to 'authRoutes' in app.js"

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import router from "./routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import auth from "./middlewares/authMiddleware.js";
 
 
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json());
 
 // Define Routes
-app.use("/api/auth", router);
+app.use("/api/auth", authRoutes);
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
