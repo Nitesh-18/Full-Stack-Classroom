a8643569b1ae322c8f5976583d76cc56cbb16ef2
Nitesh <niteshranjankar5@gmail.com>
2024-08-13 00:25:52 +0530

    Added authRoutes for registration and login. Also, added authMiddleware to check the verified user.

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import { verify } from "jsonwebtoken";
+
+const auth = (req, res, next) => {
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+export default auth;
---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { registerUser, loginUser } from "../controllers/authController";
+
+const router = Router();
+
+// Registration Route
+router.post(
+  "/register",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password must be at least 6 characters").isLength({
+      min: 6,
+    }),
+    check("role", "Role is required").isIn(["Principal", "Teacher", "Student"]),
+  ],
+  registerUser
+);
+
+// Login Route
+router.post(
+  "/login",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  loginUser
+);
+
+export default router;
