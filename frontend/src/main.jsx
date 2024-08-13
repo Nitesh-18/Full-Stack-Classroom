26b90fd89251987ff70d06671360da61bc5ef279
Nitesh <niteshranjankar5@gmail.com>
2024-08-13 16:08:21 +0530

    "Refactor App.jsx to use React Router and remove logo and counter components; move routing configuration from main.jsx to App.jsx"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,19 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import PrincipalDashboard from "./pages/PrincipalDashboard";
-import TeacherDashboard from "./pages/TeacherDashboard";
-import StudentDashboard from "./pages/StudentDashboard";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/principal" element={<PrincipalDashboard />} />
-        <Route path="/teacher" element={<TeacherDashboard />} />
-        <Route path="/student" element={<StudentDashboard />} />
-      </Routes>
-    </Router>
+    <App/>
   </React.StrictMode>
 );
