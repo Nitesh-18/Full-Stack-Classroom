8b3bd9ea1bbf67505bb25752f923e0af18447c64
Nitesh <niteshranjankar5@gmail.com>
2024-08-13 16:30:57 +0530

    "Added axios dependency, updated package-lock.json, and modified App.jsx, Navbar.jsx, main.jsx, and TeacherDashboard.jsx files"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
-import { useState } from 'react';
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css';
-import PrincipalDashboard from "./pages/PrincipalDashboard";
-import TeacherDashboard from "./pages/TeacherDashboard";
-import StudentDashboard from "./pages/StudentDashboard";
+import "./App.css";
+import PrincipalDashboard from "./pages/PrincipalDashboard.jsx";
+import TeacherDashboard from "./pages/TeacherDashboard.jsx";
+import StudentDashboard from "./pages/StudentDashboard.jsx";
+import NavBar from "./components/Navbar";
 
 function App() {
-
   return (
-    <Router>
+    <main className="bg-slate-400">
+      <Router>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<App />} />
         <Route path="/principal" element={<PrincipalDashboard />} />
         <Route path="/teacher" element={<TeacherDashboard />} />
         <Route path="/student" element={<StudentDashboard />} />
       </Routes>
     </Router>
-  )
-};
+    </main>
+  );
+}
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App.jsx";
 import "./index.css";
-import App from "./App";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
